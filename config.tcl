4493678760fc5989619c0d85b6193de261ae405c
Matt Venn <matt@mattvenn.net>
2021-06-16 12:14:30 +0200

    remove clock buffer

--- config.tcl
+++ config.tcl
@@ -11,6 +11,9 @@ set ::env(VERILOG_FILES) "$::env(DESIGN_DIR)/wrapper.v \
 # target density, change this if you can't get your design to fit
 set ::env(PL_TARGET_DENSITY) 0.4
 
+# don't put clock buffers on the outputs, need tristates to be the final cells
+set ::env(PL_RESIZER_BUFFER_OUTPUT_PORTS) 0
+
 # set absolute size of the die to 300 x 300 um
 set ::env(DIE_AREA) "0 0 300 300"
 set ::env(FP_SIZING) absolute
