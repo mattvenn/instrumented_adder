4525568d4091e79944c397929214d5343c1069f9
Matt Venn <matt@mattvenn.net>
2021-09-29 12:21:06 +0200

    remove power pins

--- config.tcl
+++ config.tcl
@@ -30,8 +30,8 @@ set ::env(DESIGN_IS_CORE) 0
 set ::env(GLB_RT_MAXLAYER) 5
 
 # define power straps so the macro works inside Caravel's PDN
-set ::env(VDD_NETS) [list {vccd1} {vccd2} {vdda1} {vdda2}]
-set ::env(GND_NETS) [list {vssd1} {vssd2} {vssa1} {vssa2}]
+set ::env(VDD_NETS) [list {vccd1}]
+set ::env(GND_NETS) [list {vssd1}]
 
 # regular pin order seems to help with aggregating all the macros for the group project
 set ::env(FP_PIN_ORDER_CFG) $script_dir/pin_order.cfg
--- wrapper.v
+++ wrapper.v
@@ -5,14 +5,8 @@
 // update this to the name of your module
 module wrapped_project(
 `ifdef USE_POWER_PINS
-    inout vdda1,	// User area 1 3.3V supply
-    inout vdda2,	// User area 2 3.3V supply
-    inout vssa1,	// User area 1 analog ground
-    inout vssa2,	// User area 2 analog ground
     inout vccd1,	// User area 1 1.8V supply
-    inout vccd2,	// User area 2 1.8v supply
     inout vssd1,	// User area 1 digital ground
-    inout vssd2,	// User area 2 digital ground
 `endif
     // wishbone interface
     input wire wb_clk_i,            // clock, runs at system clock
